cc506d83d990b8244a26fe321c44b218ffb14040
Moiseicho45 <mateizahariev@gmail.com>
2023-11-19 00:27:06 +0200

    other final stuff

--- car_handler.py
+++ car_handler.py
@@ -4,19 +4,14 @@ import time
 import random
 from datetime import datetime
 
-# Define the base URL for the API
 BASE_URL = 'http://localhost:5000'
 
-# Define the number of threads to create
 NUM_THREADS = 3
 
-# Define the radius around Sofia
 SOFIA_CENTER = (42.6977, 23.3219)
-SOFIA_RADIUS = 0.1 # in degrees
+SOFIA_RADIUS = 0.1
 
-# Define the function to send a POST request to create a new car and add a new location to it
 def create_car_and_add_location():
-    # Define the request data
     car_id = random.randint(1, 1000)
     car = {
         'id': car_id,
@@ -38,7 +33,6 @@ def create_car_and_add_location():
         
     time.sleep(120)
     while True:
-        # Define the request data
         location = {
             'coordinates': {
                 'longitude': SOFIA_CENTER[1] + random.uniform(-SOFIA_RADIUS, SOFIA_RADIUS),
@@ -47,22 +41,18 @@ def create_car_and_add_location():
             'timestamp': datetime.utcnow().isoformat(),
             'device_id': f'{car_id}'
         }
-        # Send the POST request
         response = requests.post(f'{BASE_URL}/data/{location["device_id"]}', json=location)
         if response.status_code == 200:
             print(f'Location added successfully for car with id {car_id}.')
         else:
             print(f'Error adding location for car with id {car_id}.')
-        # Wait for 2 minutes before adding new locations
         time.sleep(120)
 
 if __name__ == '__main__':
-    # Create the threads
     threads = []
     for i in range(NUM_THREADS):
         thread = threading.Thread(target=create_car_and_add_location)
         threads.append(thread)
         thread.start()
-    # Wait for the threads to finish
     for thread in threads:
         thread.join()
--- flask_app.py
+++ flask_app.py
@@ -4,63 +4,50 @@ from geopy.distance import geodesic
 
 app = Flask(__name__)
 
-# Load the data from the JSON file
 with open('data.json') as f:
     data = json.load(f)
 
-# GET all links
 @app.route('/')
 def get_links():
     return render_template('index.html')
 
-# POST new location to the given car
 @app.route('/data/<int:car_id>', methods=['POST'])
 def post_location(car_id):
-    # Get the request data
     location = request.get_json()
-    # Find the car with the given ID
     car = next((car for car in data['cars'] if car['id'] == car_id), None)
     if car:
-        # Add the new location to the car's locations list
         car['locations'].append(location)
-        # Write the updated data to the JSON file
         with open('data.json', 'w') as f:
             json.dump(data, f, indent=4)
         return jsonify({'message': 'Location added successfully.'}), 200
     else:
         return jsonify({'error': 'Car not found.'}), 404
 
-# POST a new car to the data.json file
 @app.route('/data/cars', methods=['POST'])
 def post_car():
-    # Get the request data
     car = request.get_json()
-    # Add the new car to the data
     data['cars'].append(car)
-    # Write the updated data to the JSON file
     with open('data.json', 'w') as f:
         json.dump(data, f, indent=4)
     return jsonify({'message': 'Car added successfully.'}), 200
 
-# GET a map of all the locations in order for a given car
 @app.route('/map/id/<int:car_id>')
 def map(car_id):
     with open('data.json') as f:
         data = json.load(f)
+    coordinates = []
     for car in data['cars']:
         if car['id'] == car_id:
             coordinates = [location['coordinates'] for location in car['locations']]
             print(coordinates)
             break
-        
+    
     if len(coordinates) == 0:
         return jsonify({'error': 'Car not found.'}), 404
     return render_template('map.html', coordinates=coordinates)
 
-# GET a map of the latest locations of all cars
 @app.route('/map/latest')
 def get_latest_map():
-    # Get the latest location of each car
     latest_locations = []
     for car in data['cars']:
         if car['locations']:
@@ -70,12 +57,9 @@ def get_latest_map():
         return jsonify({'error': 'Cars not found.'}), 404
     return render_template('map.html', coordinates=latest_locations)
 
-# GET the furthest location from a hardcoded one for all cars
 @app.route('/map/furthest')
 def get_furthest_map():
-    # Hardcoded location
     reference_location = (42.65234964884238, 23.354063873748185)
-    # Get the furthest location of each car
     furthest_locations = []
     for car in data['cars']:
         furthest_location = max(car['locations'], key=lambda x: geodesic(reference_location, (x['coordinates']['latitude'], x['coordinates']['longitude'])).km)
